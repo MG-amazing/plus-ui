192a7b0b972a40b09d6f1fa2795b43604bfe6aea
MG-Amazing <wang128027@gmail.com>
2025-08-04 01:49:05 +0900

    移除

--- src/composition/ListComposition.js
+++ src/composition/ListComposition.js
@@ -26,44 +26,7 @@ export default function () {
     });
   }
   getButtonsData();
-  function exportExcelData(data) {
-    data.form = pageTable.form;
-    data.columns = pageTable.columns;
 
-    // commonExportExcel(global.$http, param, data)
-    //     .then((result) => {
-    //         const blob = new Blob([result])
-    //         const downloadElement = document.createElement('a')
-    //         const href = window.URL.createObjectURL(blob) // 创建下载的链接
-    //         downloadElement.href = href
-    //         downloadElement.download = data.fileName // 下载后文件名
-    //         document.body.appendChild(downloadElement)
-    //         downloadElement.click() // 点击下载
-    //         document.body.removeChild(downloadElement) // 下载完成移除元素
-    //         window.URL.revokeObjectURL(href) // 释放掉blob对象
-    //     }).catch(() => {
-    //         ElMessage.error('导出失败')
-    //     })
-  }
-
-  function exportExcelListData(data) {
-    data.form = pageTable.form;
-    data.columns = pageTable.columns;
-    // commonExportListExcel(global.$http, data)
-    //     .then((result) => {
-    //         const blob = new Blob([result])
-    //         const downloadElement = document.createElement('a')
-    //         const href = window.URL.createObjectURL(blob) // 创建下载的链接
-    //         downloadElement.href = href
-    //         downloadElement.download = data.fileName // 下载后文件名
-    //         document.body.appendChild(downloadElement)
-    //         downloadElement.click() // 点击下载
-    //         document.body.removeChild(downloadElement) // 下载完成移除元素
-    //         window.URL.revokeObjectURL(href) // 释放掉blob对象
-    //     }).catch(() => {
-    //     ElMessage.error('导出失败')
-    // })
-  }
 
   return {
     pageTable
